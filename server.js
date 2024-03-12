6a275b37845a3a0b07e3ea32dcbd436d7a26d386
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-03-12 09:12:43 +0100

    Add server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const messages = []
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/index.html'));
+});
+
+app.use((req, res) => {
+    res.status(404).json({ message: 'Not found...' });
+});
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running on port: 8000');
+});
